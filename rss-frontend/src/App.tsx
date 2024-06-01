db8ce3e78063af74857596ea88dbf344013c4b68
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-06-01 23:53:31 +0800

    Added an automatic refresh feature to the feed so the feed refreshes automatically every 30 minutes

--- rss-frontend/src/App.tsx
+++ rss-frontend/src/App.tsx
@@ -1,32 +1,39 @@
 import Channel from "./Channel.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 
 function App() {
     const [channels, setChannels] = useState<Channel[]>([]);
-
-    useEffect(() => {
-        let channels: Channel[] = [];
-
+    const refreshFeed = useCallback( () => {
         const url: string = "http://127.0.0.1:5000";
         fetch(url).then(r => {
             if (r.ok) {
                 return r.json();}
         }).then((data) => {
-            for (let i = 0; i < data.length; i++) {
-                channels.push(data[i]);
-            }
-            setChannels(channels);
+            console.log("Fetched channels" + data.map((channel: Channel) => {return " " + channel.title}));
+            setChannels(data);
         }).catch((error: Error) => console.log(error))
+    }, [setChannels]);
 
+    useEffect(() => {
+        refreshFeed()
     }, [setChannels])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            refreshFeed();
+            console.log("Refreshed feed");
+        }, 1.8e6);
+
+        return () => clearInterval(interval);
+    }, [])
 
     return (
         <>
             <h1>Dingbat</h1>
             <div className={"feed"}>
                 {channels.map(channel => {
-                    return <Channel key={channel.rss_url}
+                    return <Channel key={channel.title + channel.description}
                                     title={channel.title}
                                     url={channel.url}
                                     rss_url={channel.rss_url}
