bbc774de179ffd308c7c53071557269bf9e41a73
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-06-04 12:41:06 +0800

    Added API documentation

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ TODO:
 ## TODO
 - [ ] Finish readme
 - [X] Add track/untrack channels feature
-- [ ] Add API endpoint documentation in app.py
+- [X] Add API endpoint documentation in app.py
 - [ ] Styling
--- rss-api/app.py
+++ rss-api/app.py
@@ -22,14 +22,24 @@ interactor = Interactor(DAO, build_RSS(urls), 5)
 
 @app.route('/')
 def main():
-    """TODO:"""
+    """Responds to a GET request by returning the list of tracked channels and their stories. This rebuilds the stories
+    in the tracked channels, and returns a serialised JSON array of the channels and their respective stories.
+    """
     interactor.build_channels()
     return jsonify([channel.serialize() for channel in interactor.channels])
 
 
 @app.post('/channels/')
 def add_channel():
-    """TODO:"""
+    """Responds to a POST request attempting to start tracking a channel.
+
+    Requires
+        - Parameters "name", "url". The name must not match any currently tracked channels, and the url must be a valid
+          link to an RSS document.
+    Returns
+        - On success, a Response with status 201 and a message detailing that the channel has been added. On
+          failure, a Response with status 400 and a message detailing the failure.
+    """
     data = request.form
     if interactor.add_channel(data["name"], data["url"]):
         interactor.build_channels()
@@ -40,7 +50,14 @@ def add_channel():
 
 @app.delete('/channels/')
 def remove_channel():
-    """TODO:"""
+    """Responds to a DELETE request attempting to stop tracking a channel.
+
+    Requies
+        - A API request with the name of the channel to be deleted as the body.
+    Returns
+        - On success, a Response with status 204 and a message detailing the deletion. On failure, a Response with
+          status 400 and a message detailing the failure to delete.
+    """
     name = request.data.decode("utf-8")
     if interactor.remove_channel(name):
         interactor.build_channels()
@@ -51,7 +68,13 @@ def remove_channel():
 
 @app.put('/storyCap/')
 def set_max_stories():
-    """TODO:"""
+    """Respond to a PUT request setting a new max story limit.
+
+    Requires
+        - Parameter "num": The new value of the story limit, which must be an integer.
+    Returns
+        - a Response with status 200.
+    """
     interactor.max_stories = int(request.form["num"])
     interactor.build_channels()
     return Response(status=200)
